d2660728b47d24f6d19efc1933fd5243aa5bc711
dsuccar <danielsuccar@icloud.com>
2020-04-30 21:07:14 -0400

    4.30 working on submit fetch

--- app/controllers/user_questions_controller.rb
+++ app/controllers/user_questions_controller.rb
@@ -6,5 +6,18 @@ class UserQuestionsController < ApplicationController
         # options[:include] = [:user, :question]
         render json: UserQuestion.all.to_json(:include => [:user, :question])
     end
-end
 
+
+    def create
+    #   byebug
+        user_question = UserQuestion.create(points: 1, user_id: params[:user_id], question_id: params[:question_id])
+
+
+    end
+end
+# def create
+#     name = Faker::Name.first_name
+#     species = Faker::Games::Pokemon.name
+#     new_pokemon = Pokemon.create(nickname: name, species: species, trainer_id: params[:trainer_id])
+#     render json: new_pokemon
+# end
