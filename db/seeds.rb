26a578ac4c0d39b6af833ce17b9007c4df3ca4fc
kaidornel <kai_dornel@hotmail.com>
2020-05-01 19:00:44 -0400

    commented out most seed data to fix loading times

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,13 +40,15 @@ resp18 = RestClient.get 'https://opentdb.com/api.php?amount=67'
 resp19 = RestClient.get 'https://opentdb.com/api.php?amount=68'
 resp20 = RestClient.get 'https://opentdb.com/api.php?amount=69'
 
-def cleaner(argument)
-    gsub1 = argument.gsub("&quot;", "'")
+def cleaner(argument) #"[\"Valpara&iacute;so\", \"Copiap&oacute;\", \"Antofagasta\"]" is another issue
+    gsub1 = argument.gsub("&quot", "'")
     gsub2 = gsub1.gsub("&#039;", "'")
     gsub3 = gsub2.gsub("&amp", "&")
     return gsub3
 end
 
+# ok. Maybe i'll get a beer soon then
+# arr.map{|x| cleaner(x)}
 json1 = JSON.parse(resp1.body)
 json2 = JSON.parse(resp2.body)
 json3 = JSON.parse(resp3.body)
@@ -68,8 +70,6 @@ json18 = JSON.parse(resp18.body)
 json19 = JSON.parse(resp19.body)
 json20 = JSON.parse(resp20.body)
 
-
-
 # function stringFixer1(string){
 #   return string.replace(/&quot;/g, "'")
 # }
@@ -82,34 +82,29 @@ json20 = JSON.parse(resp20.body)
 #   return string.replace(/&amp/g, "&")
 # }
 
-
 #100.times do
-i50 = json1["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-ii50 = json2["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-iii50 = json3["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-iv50 = json4["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-v50 = json5["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-vi50 = json6["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-vii50 = json7["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-viii50 = json8["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-ix50 = json9["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-x50 = json10["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-xi50 = json11["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-xii50 = json12["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-xiii50 = json13["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-xiv50 = json14["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-xv50 = json15["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-xvi50 = json16["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-xvii50 = json17["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-xviii50 = json18["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-xix50 = json19["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
-xx50 = json20["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
 
-byebug
+i50 = json1["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# ii50 = json2["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# iii50 = json3["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# iv50 = json4["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# v50 = json5["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# vi50 = json6["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# vii50 = json7["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# viii50 = json8["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# ix50 = json9["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# x50 = json10["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# xi50 = json11["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# xii50 = json12["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# xiii50 = json13["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# xiv50 = json14["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# xv50 = json15["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# xvi50 = json16["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# xvii50 = json17["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# xviii50 = json18["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# xix50 = json19["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
+# xx50 = json20["results"].each{|question| Question.create(question_text: question["question"], correct_answer: question["correct_answer"], incorrect_answers: question["incorrect_answers"])}
 
 userquestion1 = UserQuestion.create(points: 1, user_id: user1.id, question_id: question2.id)
 userquestion2 = UserQuestion.create(points: 1, user_id: user2.id, question_id: question1.id)
 
-# byebug
-json1["results"].each{|question| Question.create(question_text: cleaner(question["question"]), correct_answer: cleaner(question["correct_answer"]), incorrect_answers: cleaner(question["incorrect_answers"]))}
-json1["results"].each{|question| Question.create(question_text: cleaner(question["question"]), correct_answer: cleaner(question["correct_answer"]), incorrect_answers: question["incorrect_answers"])}
