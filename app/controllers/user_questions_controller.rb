6e79fb2840dc5b322c055eb139f84058bea1b159
kaidornel <kai_dornel@hotmail.com>
2020-05-02 12:49:52 -0400

    added create action

--- app/controllers/user_questions_controller.rb
+++ app/controllers/user_questions_controller.rb
@@ -9,10 +9,7 @@ class UserQuestionsController < ApplicationController
 
 
     def create
-
         user_question = UserQuestion.create(points: 1, user_id: params[:user_id], question_id: params[:question_id])
-
-
     end
 end
 # def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,25 @@ class UsersController < ApplicationController
         # render json: users: include: [:name]
         render json: User.all.to_json(:include => :user_questions) #may not need the include clause
     end
+    
+    def show
+        user = User.find_by(name: params[:name])
+        byebug
+    end
+
+    def create
+        # byebug
+        # begin
+        #     user = User.find(params[:name])
+        # rescue
+            user = User.create(name: params[:name] )#, user_questions: params[:user_question])
+        # else
+            render json: user
+        # end
+    end
 end
 
-
+# user_question = UserQuestion.create(points: 1, user_id: params[:user_id], question_id: params[:question_id])
 
 
 # class TrainersController < ApplicationController
